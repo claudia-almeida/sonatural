18b3ecdd3377f945c0b8e2324ccf5efa488eb823
Claudia Almeida <ihavenids@gmail.com>
2016-01-25 23:10:10 +0000

    Added Edit and Update functionality with Devise Gem. It was amazing.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -17,6 +17,16 @@ class PlacesController < ApplicationController
 		@place = Place.find(params[:id])
 	end
 
+	def edit
+		@place = Place.find(params[:id])
+	end
+
+	def update
+		@place = Place.find(params[:id])
+		@place.update_attributes(place_params)
+		redirect_to root_path
+	end
+
 	private
 
 	def place_params
