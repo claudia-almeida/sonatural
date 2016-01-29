0c8d74a95a1ce358263d947bb388210855be9bd6
Claudia Almeida <ihavenids@gmail.com>
2016-01-29 02:49:31 +0000

    Email notifications are up and running

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,8 +1,11 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@sonatural.com"
 
-  def comment_added
-  	mail(to: "tfpclaudia@gmail.com",
-  		subject: "A comment has been added to one of your places!")
+  def comment_added(comment)
+  	@place = comment.place
+  	@place_owner = @place.user
+
+  	mail(to: @place_owner.email,
+  		subject: "A comment has been added to #{@place.name}")
   end
 end
