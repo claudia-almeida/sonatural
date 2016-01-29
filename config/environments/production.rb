549967969db64c94899c71555436be28f0885371
Claudia Almeida <ihavenids@gmail.com>
2016-01-29 04:33:56 +0000

    Added Mailgun to Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Nomster::Application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'yourapp.heroku.com',
+    :domain         => 'sonatural-claudia.herokuapp.com',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
