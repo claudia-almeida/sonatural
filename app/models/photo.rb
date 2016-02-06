3199911d4c898cc903dd47057a866fb8fb83b0ee
Claudia Almeida <ihavenids@gmail.com>
2016-02-06 20:23:49 +0000

    More styling and added picture to homepage places

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,5 @@ class Photo < ActiveRecord::Base
 	mount_uploader :picture, PictureUploader
 	belongs_to :user
 	belongs_to :place
+	validates :picture, presence: true
 end
--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 
 class PictureUploader < CarrierWave::Uploader::Base
-
+  
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
